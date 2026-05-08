fe0ac3a7bc28761531402307f2a9e91b7ab485a9
haakonhalvors1 <haakonhalv04@gmail.com>
2026-05-08 14:55:00 +0200

    a

--- obj/Debug/net9.0/FestivalMvc.AssemblyInfo.cs
+++ obj/Debug/net9.0/FestivalMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FestivalMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da4af47722b9e4bff973633cfc94012f736ec2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21ab08864caaa3ff75ba0cb061bcfb723a590822")]
 [assembly: System.Reflection.AssemblyProductAttribute("FestivalMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FestivalMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
