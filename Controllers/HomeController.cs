67c8b332f477718c0c73fe5ff2aa53134843edc6
haakonhalvors1 <haakonhalv04@gmail.com>
2026-05-08 15:14:47 +0200

    s

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,7 +6,43 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        ViewData["Title"] = "Nordlys Festival 2026";
+        ViewData["Title"] = "Landstreffet Stavanger 2026";
+        return View();
+    }
+
+    public IActionResult Billetter()
+    {
+        ViewData["Title"] = "Kjøp Billetter - Landstreffet Stavanger";
+        return View();
+    }
+
+    public IActionResult Fordeler()
+    {
+        ViewData["Title"] = "LS-Fordeler - Landstreffet Stavanger";
+        return View();
+    }
+
+    public IActionResult Info()
+    {
+        ViewData["Title"] = "LS-Info - Landstreffet Stavanger";
+        return View();
+    }
+
+    public IActionResult Artister()
+    {
+        ViewData["Title"] = "Artister - Landstreffet Stavanger";
+        return View();
+    }
+
+    public IActionResult Samarbeid()
+    {
+        ViewData["Title"] = "Samarbeid - Landstreffet Stavanger";
+        return View();
+    }
+
+    public IActionResult LS2027()
+    {
+        ViewData["Title"] = "LS2027 - Landstreffet Stavanger";
         return View();
     }
 }
